1ead9926c5f379f15d5d5ca9a56cf00951fcb3be
Ify Charmaine N <152789511+IfyCharmaineN@users.noreply.github.com>
2024-03-11 18:32:42 +0000

    finance_calculator.py

--- finance_calculator.py
+++ finance_calculator.py
@@ -17,18 +17,18 @@ print("-"*90)
 print("To calculate the amount of interest you'll earn on your investment enter 'Investment'")
 print("\nTo calculate the amount you'll pay on a home loan enter 'Bond'\n")
 print("-"*90)
-#Created a variable called 'service' for user to input 'Investment' or 'Bond'
+# Created a variable called 'service' for user to input 'Investment' or 'Bond'
 
 service = input("\nPlease enter 'Investment' or 'Bond' : ")
 
-#if user enters 'investment' ask user to input following:
+# if user enters 'investment' ask user to input following:
 
-    #how much they are depositing,
-    #at what interest rate (%),
-    #the number of years they plan on investing 
-    #'Simple' or 'Compound' interest
+    # how much they are depositing,
+    # at what interest rate (%),
+    # the number of years they plan on investing 
+    # 'Simple' or 'Compound' interest
 
-    #.lower() is used  to make sure all str user inputs aren't case sensitive
+    # .lower() is used  to make sure all str user inputs aren't case sensitive
 
 if service.lower() == "investment" :
     deposit_amount = int(input("\nEnter the amount you're depositing: "))
@@ -36,7 +36,7 @@ if service.lower() == "investment" :
     years_interest = int(input("\nEnter number of years you plan on investing : "))
     interest = input("\nPlease enter 'Simple' or 'Compound' interest: ")   
      
-    #Simple interest formula:
+    # Simple interest formula:
 
     P = deposit_amount
     r = user_interest/100
@@ -45,7 +45,7 @@ if service.lower() == "investment" :
     A = P*(1+r*t)
     simple = A
     
-    #Compound interest formula:
+    # Compound interest formula:
 
     A = P*math.pow((1+r),t)
 
